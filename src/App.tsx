fb03bb5274f6b26ca5867de3ae858d8e2a105212
Onder Bakirtas <onderbakirtas@gmail.com>
2023-11-13 23:29:23 +0300

    feat: added device detect hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import RootLayout from "./layouts/RootLayout";
 import Home from "./views/Home";
 import Products from "./views/Products";
 import Chat from "./views/Chat";
+import useDeviceDetection from "./hooks/useDeviceDetect";
 
 const App = () => {
+  const device = useDeviceDetection();
+  
+
   return (
     <div>
       <h1>React PWA</h1>
+
+      <h2>{device}</h2>
 
       <Routes>
         <Route path="/" element={<RootLayout />}>
